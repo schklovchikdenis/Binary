b412b6a37615a6c9c27fb861e42050af337b42e2
Денис <denis@iMac-Denis.local>
2020-04-07 19:59:38 +0500

    Portfolio end

--- #source/js/script.js
+++ #source/js/script.js
@@ -285,4 +285,18 @@ function mainblock(){
 		var h=$(window).outerHeight();
 	$('.mainblock').css('min-height',h);
 }
-	mainblock();
+mainblock();
+
+$('.filter__item').click(function(event) { 
+	var i=$(this).data('filter');
+	if (i ==1) {
+		$('.portfolio__column').show();
+	} else {
+		$('.portfolio__column').hide();
+		$('.portfolio__column.f_'+i).show();
+	}
+	$('.filter__item').removeClass('active');
+	$(this).addClass('active');
+
+	return false;
+});
